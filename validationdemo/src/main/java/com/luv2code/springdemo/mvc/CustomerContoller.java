2723c3432155e3507ceb239a8e55f5d71b8d6738
Arka <arkamaity212@gmail.com>
2024-07-07 20:39:40 +0530

    Add @InitBinder to remove the white space before it got to the controller

--- validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerContoller.java
+++ validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerContoller.java
@@ -1,9 +1,12 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,6 +14,21 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerContoller {
+
+    //Add @initBinder -- to convert trim input strings(basically remove the whitespace)
+    //resolve issue for validation it remove the leading and trailing whitespace.
+
+    @InitBinder
+    public void InitBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        //here String.class means-- apply for all String where spring find a white space it will remove it.
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+        
+
+    }
+
 
     @GetMapping("/")
     public String showForm(Model theModel){ // model allows us to share the data between the controllers anf view pages(html pages --- thymeleaf)
@@ -27,6 +45,8 @@ public class CustomerContoller {
         //@Valid --Tell Spring MVC to perform validation
         //@ModelAttribute -- Read the form data form model aattribute customer when user submitted the form
         //bindingResult -- Hold the result of the validation.
+
+        System.out.println("Last Name: |"+ thecustomer.getLastName()+" |");
 
         if(theBindingResult.hasErrors()){
             return "customer-form";
