389ca6824007e7b434b47f8775e371fde2fe7d15
Arka <arkamaity212@gmail.com>
2024-07-09 08:42:32 +0530

    Adding number and regular expression validation code in customer.java and customer-form.html

--- validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,15 +1,25 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
 
     private String firstName;
-    
+
     @NotNull(message = "is Required")
     @Size(min=1,message = "is Required") //minimun 1 character should be there or it will give an error
     private String lastName;
+
+    @Min(value=0,message="must be greater than or equal to 0")
+    @Max(value=10,message ="must be less than or equal to 10")
+    private int freePasses;
+
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message = "must be 5 chras/digits")
+    private String postalCode;
 
 
     public String getFirstName() {
@@ -26,6 +36,22 @@ public class Customer {
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
     
 }
